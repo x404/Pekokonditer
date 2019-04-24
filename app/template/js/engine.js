aa9590d1f03c81a06231822f4087d6dbbc178eda
Aleksey <kronos2003@gmail.com>
2019-04-25 01:01:38 +0300

    Группы товаров на главной, подключен слайдер

--- app/template/js/engine.js
+++ app/template/js/engine.js
@@ -1,6 +1,70 @@
 $(document).ready(function(){
 
 
+	$('#groups-carousel2').slick({
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		arrows: false,
+		fade: true,
+		asNavFor: '#groups-carousel'
+	});
+
+
+	$('#groups-carousel').slick({
+		slidesToShow: 7,
+		arrows: false,
+		slidesToScroll: 1,
+		dots: true,
+		asNavFor: '#groups-carousel2',
+		focusOnSelect: true,
+		infinite: false,
+		responsive: [
+			{
+				breakpoint: 1400,
+				settings: {
+					slidesToShow: 4,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 1250,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 992,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 991,
+				settings: {
+					slidesToShow: 5,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 768,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 1
+				}
+			},
+			{
+				breakpoint: 450,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 1
+				}
+			}
+		]
+	});
+
+
 	// mobile-menu
 	$('#navbar').each(function(){
 		var $this = $(this),
